6631826cdd0c418a69fe5a0dcc72dbc1046bfcee
Rhoopes25 <rachhoopes1@gmail.com>
2025-02-09 18:51:57 -0700

    added in main for each page

--- src/app.jsx
+++ src/app.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Breathe } from './breathe/breathe';
+import { Rate } from './rate/rate';
+import { Journal } from './journal/journal';
+
 export default function App() {
     return (
+     <BrowserRouter>
       <div className='body bg-dark text-light'>
         <header>
         <h1>Emotional Check-In</h1>
         <nav>
-          <a href="index.html">Login page</a>
-          <a href="rate.html">Log Emotions</a>
-          <a href="journal.html">Journal</a>
-        </nav>
+            <NavLink className='nav-link' to='/login'>Login Page</NavLink>
+            <NavLink className='nav-link' to='/rate'>Log Emotions</NavLink>
+            <NavLink className='nav-link' to='/journal'>Journal</NavLink>
+            <NavLink className='nav-link' to='/breathe'>Breathe</NavLink>
+          </nav>
       </header>
   
-        <main>App components go here</main>
+        <main>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/rate' element={<Rate />} />
+            <Route path='/journal' element={<Journal />} />
+            <Route path='/breathe' element={<Breathe />} />
+            <Route path='/play' element={<h2>Play Page</h2>} />
+          </Routes>
+        </main>
   
         <footer className='custom-footer'>
           <div className='container-fluid'>
@@ -25,5 +41,11 @@ export default function App() {
           </div>
         </footer>
       </div>
+      </BrowserRouter>
     );
+  }
+
+
+  function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
---
+++ src/breathe/breathe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Breathe() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>breathe displayed here</div>
+    </main>
+  );
+}
